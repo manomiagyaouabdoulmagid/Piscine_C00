7b31d784cd4eb3401f50a054d812431899a91316
manomiagyaouabdoulmagid <cnoniger@gmail.com>
2025-05-13 08:06:40 +0100

    Update check_file_exist.sh

--- check_file_exist.sh
+++ check_file_exist.sh
@@ -3,7 +3,9 @@
 
 read -p "Entrez le nom du fichier : " filename
 
-if [ -f "$filename" ]; then
+if [ -z "$filename" ]; then
+    echo "Erreur : aucun fichier n'a été entré."
+elif [ -f "$filename" ]; then
     echo "Le fichier '$filename' existe."
 else
     echo "Le fichier '$filename' n'existe pas."
