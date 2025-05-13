d6b7a52e236e67f85afb33a10d403153c139ae18
manomiagyaouabdoulmagid <cnoniger@gmail.com>
2025-05-13 08:04:03 +0100

    Update current_datetime.sh

--- current_datetime.sh
+++ current_datetime.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-# Affiche la date et l'heure actuelle
+# Affiche la date et l'heure actuelle au format "YYYY-MM-DD HH:MM:SS"
 
-date "+%Y-%m-%d %H:%M:%S"
+if ! date_output=$(date "+%Y-%m-%d %H:%M:%S"); then
+  echo "Erreur lors de la récupération de la date."
+else
+  echo "$date_output"
+fi
