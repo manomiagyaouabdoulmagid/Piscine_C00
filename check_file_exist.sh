88dbe37a4e8c4db58c3d26a9e2c5ac7969fb3a6f
manomiagyaouabdoulmagid <cnoniger@gmail.com>
2025-05-13 07:34:08 +0100

    Create check_file_exist.sh

---
+++ check_file_exist.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Vérifie si un fichier donné existe
+
+read -p "Entrez le nom du fichier : " filename
+
+if [ -f "$filename" ]; then
+    echo "Le fichier '$filename' existe."
+else
+    echo "Le fichier '$filename' n'existe pas."
+fi
