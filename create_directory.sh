0b0cebaa61d740648feb0903dd2d87f6551beb59
manomiagyaouabdoulmagid <cnoniger@gmail.com>
2025-05-13 08:05:29 +0100

    Update create_directory.sh

--- create_directory.sh
+++ create_directory.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-# Crée un répertoire nommé test_directory
+# Crée un répertoire nommé test_directory s'il n'existe pas
 
-mkdir -p test_directory
-echo "Répertoire créé."
+if [ -d test_directory ]; then
+  echo "Le répertoire 'test_directory' existe déjà."
+else
+  mkdir test_directory && echo "Répertoire 'test_directory' créé."
+fi
